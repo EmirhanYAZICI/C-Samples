427b60711921fe0712a66e9d1dd0c13da1263714
Emirhan Yazıcı <emirhanyzc3429@gmail.com>
2025-11-29 17:37:06 +0300

    Add files via upload

---
+++ system .cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class using System;
+{
+    public using system()
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("This is a sample C# class.");
+
+        Console.WriteLine("It demonstrates basic syntax and structure.");
+
+        Console.WriteLine("Goodbye, World!");
+
+        Console.WriteLine("End of the class demonstration.");
+    }
+}
